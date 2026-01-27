d1f09235018389223d0cbb1db1185c9f051e8c2b
Olga Kozlova <olgakozlova.web@gmail.com>
2026-01-27 22:53:23 +0300

    day2

---
+++ week1/day2/code/1.py
@@ -0,0 +1,48 @@
+# a = 1
+# print(id(a))
+# type_a = type(a)
+# print(type_a)
+# print(type(type_a))
+# print(isinstance(a, str))
+# print(isinstance(a, int))
+# print(isinstance(a, (bool, str)))
+
+# def f(x):
+#     x = x + [4]
+
+# lst = [1, 2, 3]
+# f(lst)
+# print(lst)
+
+# def f(x):
+#     x += [4]
+
+# lst = [1, 2, 3]
+# f(lst)
+# print(lst)
+
+# a = [1, 2, 3]
+# b = a
+
+# b[:] = [9, 9]
+
+# print(a)
+# print(a is b)
+
+# x = 10
+
+# def f():
+#     x = 20
+
+# f()
+# print(x)
+
+# x = 1
+
+# def outer():
+#     x = 2
+#     def inner():
+#         print(x)
+#     inner()
+
+# outer()
