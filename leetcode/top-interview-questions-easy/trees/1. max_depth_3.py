ced71511e31ec100553beaf1e8ade96869f7ecd6
Olga Kozlova <olgakozlova.web@gmail.com>
2026-02-14 15:09:55 +0300

    day35

--- leetcode/top-interview-questions-easy/trees/1. max_depth_3.py
+++ leetcode/top-interview-questions-easy/trees/1. max_depth_3.py
@@ -11,7 +11,6 @@ class Solution:
             return 0
         
         left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
         
-        
-        
-        
+        return 1 + max(left_depth, right_depth)
